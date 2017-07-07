711cfc3adc1a0d43d226de86403ac29daa357c3f
Yusuke Niitani <yuyuniitani@gmail.com>
2017-07-07 19:09:39 +0900

    make pca_lightning to work with cupy arrays

--- chainercv/transforms/image/pca_lighting.py
+++ chainercv/transforms/image/pca_lighting.py
@@ -1,4 +1,4 @@
-import numpy as np
+import chainer
 
 
 def pca_lighting(img, sigma, eigen_value=None, eigen_vector=None):
@@ -14,37 +14,37 @@ def pca_lighting(img, sigma, eigen_value=None, eigen_vector=None):
     NIPS 2012.
 
     Args:
-        img (~numpy.ndarray): An image array to be augmented. This is in
+        img (array): An image array to be augmented. This is in
             CHW and RGB format.
         sigma (float): Standard deviation of the Gaussian. In the original
             paper, this value is 10% of the range of intensity
             (25.5 if the range is :math:`[0, 255]`).
-        eigen_value (~numpy.ndarray): An array of eigen values. The shape
+        eigen_value (array): An array of eigen values. The shape
             has to be :math:`(3,)`. If it is not specified, the values computed
             from ImageNet are used.
-        eigen_vector (~numpy.ndarray): An array of eigen vectors. The shape
+        eigen_vector (array): An array of eigen vectors. The shape
             has to be :math:`(3, 3)`. If it is not specified, the vectors
             computed from ImageNet are used.
 
     Returns:
-        An image in CHW format.
+        array: An image in CHW format.
     """
+    xp = chainer.cuda.get_array_module(img)
 
     if sigma <= 0:
         return img
 
     # these values are copied from facebook/fb.resnet.torch
     if eigen_value is None:
-        eigen_value = np.array((0.2175, 0.0188, 0.0045))
+        eigen_value = xp.array((0.2175, 0.0188, 0.0045))
     if eigen_vector is None:
-        eigen_vector = np.array((
+        eigen_vector = xp.array((
             (-0.5675, -0.5808, -0.5836),
             (0.7192, -0.0045, -0.6948),
             (0.4009, -0.814,  0.4203)))
 
-    alpha = np.random.normal(0, sigma, size=3)
+    alpha = xp.random.normal(0, sigma, size=3)
 
-    img = img.copy()
-    img += eigen_vector.dot(eigen_value * alpha).reshape(-1, 1, 1)
+    img = img + eigen_vector.dot(eigen_value * alpha).reshape(-1, 1, 1)
 
     return img
